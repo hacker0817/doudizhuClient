01a8064a9206965b9a20684602f6246baf411215
渺小的程序员 <magicwuyujun@126.com>
2022-05-21 12:29:29 +0800

    完善注册与登录

--- website/js/login.js
+++ website/js/login.js
@@ -1,17 +1,40 @@
-var baseUrl = "http://192.168.1.52:5050";
+var baseUrl = "http://192.168.0.212:5050";
 
-$(function() {
-    $("#register").on("click", function() {
+$(function () {
+    $("#register").on("click", function () {
         let name = $("#name").val();
         let pass1 = $("#pass1").val();
         let pass2 = $("#pass2").val();
         let email = $("#email").val();
         register(name, pass1, pass2, email);
     });
+
+    $("#login").on("click", function () {
+        let user = $("#user").val();
+        let pass = $("#pass").val();
+        login(user, pass);
+    });
 });
 
 function register(name, password, repeatpwd, email) {
-    if (password != repeatpwd) alert("密码不同！");
+    if (name.trim() == "") {
+        pxmu.fail("username cannot be empty");
+        return;
+    }
+
+    if (password.trim() == "") {
+        pxmu.fail("password cannot be empty");
+        return;
+    }
+    if (password != repeatpwd) {
+        pxmu.fail("the entered passwords are inconsistent");
+        return;
+    }
+
+    if (email.trim() == "") {
+        pxmu.fail("email cannot be empty");
+        return;
+    }
 
     let param = {
         name: name,
@@ -19,38 +42,64 @@ function register(name, password, repeatpwd, email) {
         email: email,
     };
 
+    pxmu.loading({
+        close: false
+    });
     $.ajax({
         url: baseUrl + "/User?action=register",
         data: JSON.stringify(param),
         type: "POST",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function(data) {
-            console.log(data);
+        success: function (result, status, xhr) {
+            if (result.msg == "")
+                pxmu.success("register success");
+            else
+                pxmu.fail(result.msg);
+            pxmu.closeload(1000);
         },
-        error: function(err) {
-            alert(err);
+        error: function (xhr, status, error) {
+            pxmu.success("network error");
+            pxmu.closeload(1000);
         },
     });
 }
 
 function login(name, password) {
+    if (name.trim() == "") {
+        pxmu.fail("username cannot be empty");
+        return;
+    }
+    if (password.trim() == "") {
+        pxmu.fail("password cannot be empty");
+        return;
+    }
+
     let param = {
         name: name,
         password: password,
     };
 
+    pxmu.loading({
+        close: false
+    });
     $.ajax({
         url: baseUrl + "/User?action=login",
         data: JSON.stringify(param),
         type: "POST",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function(data) {
-            console.log(data);
+        success: function (result, status, xhr) {
+            if (result.msg == "")
+                window.location.href = "./doudizhuClient/index.html";
+            // pxmu.success("login success");
+            else
+                pxmu.fail(result.msg);
+            pxmu.closeload(1000);
         },
-        error: function(err) {
-            alert(err);
+        error: function (xhr, status, error) {
+            pxmu.success("network error");
+            pxmu.closeload(1000);
         },
     });
 }
