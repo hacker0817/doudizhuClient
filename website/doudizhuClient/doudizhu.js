5c2237822a03db5630c74dfd4ca7c3024b4b89c0
渺小的程序员 <magicwuyujun@126.com>
2022-05-24 21:10:00 +0800

    添加signarR访问服务端

--- website/doudizhuClient/doudizhu.js
+++ website/doudizhuClient/doudizhu.js
@@ -1,8 +1,35 @@
+const connection = new signalR.HubConnectionBuilder().withUrl("http://192.168.1.52:5050/ws", options => {
+    options.AccessTokenProvider = () => Task.FromResult("111");
+}).configureLogging(signalR.LogLevel.Information).build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+}
+
+connection.onclose(async() => {
+    await start();
+});
+
+$(function() {
+    var token = getCookie("token");
+    if (token != "") {
+        readyGame(token);
+        DJDDZ.Init("canvas1");
+    } else
+        window.location.replace("http://192.168.0.212/web/index.html");
+});
+
 function readyGame(token) {
     let param = {};
 
     pxmu.loading({
-        close: false
+        close: false,
     });
     $.ajax({
         url: baseUrl + "/Doudizhu?action=readyGame",
@@ -11,17 +38,16 @@ function readyGame(token) {
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         beforeSend: function(request) {
-            request.setRequestHeader("Authorization","Bearer " + token);
-         },
-        success: function (result, status, xhr) {
+            request.setRequestHeader("Authorization", "Bearer " + token);
+        },
+        success: function(result, status, xhr) {
             if (result.msg == "") {
-                console.log(result);            
-            }
-            else
-                pxmu.fail(result.msg);
+                console.log(result);
+                start();
+            } else pxmu.fail(result.msg);
             pxmu.closeload(1000);
         },
-        error: function (xhr, status, error) {
+        error: function(xhr, status, error) {
             pxmu.success("network error");
             pxmu.closeload(1000);
         },
