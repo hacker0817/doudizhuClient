548a01d1db7aee864bf867c61a7e8b931ad7c917
渺小的程序员 <magicwuyujun@126.com>
2022-05-25 23:43:13 +0800

    消息通信，待修改

--- website/doudizhuClient/doudizhu.js
+++ website/doudizhuClient/doudizhu.js
@@ -6,21 +6,47 @@ if (token != "") {
     window.location.replace(baseUrl + "/web/index.html");
 
 const connection = new signalR.HubConnectionBuilder()
-    .withUrl(baseUrl + "/gamews", { accessTokenFactory: () => token })
+    .withUrl(baseUrl + "/gamews", {
+        accessTokenFactory: () => token
+    })
     .build();
 
 async function start() {
     try {
         await connection.start();
         console.log("SignalR Connected.");
+        SendMessageToServer("ready");
     } catch (err) {
         console.log(err);
         setTimeout(start, 5000);
     }
 }
 
-connection.onclose(async() => {
+connection.on("ReceiveMessage", (user, message) => {
+    console.log(user);
+    console.log(message);
+});
+
+connection.onclose(async () => {
     await start();
 });
+
+async function SendMessageToServer(message) {
+    try {
+        await connection.invoke("SendMessageToServer", message);
+        console.log("SendMessageToServer.");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function SendMessageToUser(user, message) {
+    try {
+        await connection.invoke("SendMessageToUser", user, message);
+        console.log("SendMessageToUser.");
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 start();
