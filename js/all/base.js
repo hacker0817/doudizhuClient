2d62a30d20c41c4625c2acd765a88366d90c69f4
渺小的程序员 <magicwuyujun@126.com>
2022-06-02 22:37:30 +0800

    调整

--- js/all/base.js
+++ js/all/base.js
@@ -1,6 +1,10 @@
-var baseUrl = "http://192.168.0.212:5050";
-var connection;
+// var baseUrl = "http://192.168.0.212:5050";
+// var httpUrl = "http://192.168.0.212";
 // var baseUrl = "http://192.168.1.52:5050";
+// var httpUrl = "http://192.168.1.52";
+var baseUrl = "http://192.168.0.102:5050";
+var httpUrl = "http://192.168.0.102";
+var connection;
 
 // 设置cookie
 function setCookie(name, value) {
--- js/ddz/doudizhu.js
+++ js/ddz/doudizhu.js
@@ -1,6 +1,49 @@
+var roomId = getCookie("roomId");
+console.log("roomId:" + roomId);
 var token = getCookie("token");
 
-if (token != "") {
+connection = new signalR.HubConnectionBuilder()
+    .withUrl(baseUrl + "/gamews", {
+        accessTokenFactory: () => token,
+    })
+    .build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+        JoinGame();
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+}
+
+async function JoinGame() {
+    try {
+        await connection.invoke("JoinGame", roomId);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function GameStart() {
+    try {
+        await connection.invoke("GameStart", gameInfo);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+connection.onclose(() => {
+    console.log("SignalR Closed.");
+});
+
+if (roomId != "") {
+    start();
+
+
     DJDDZ.Init("canvas1");
-} else
-    window.location.replace("http://192.168.0.212/web/index.html");
+} else {
+    window.location.replace(httpUrl + "/web/index.html");
+}
